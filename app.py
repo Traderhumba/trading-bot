6541c7a4473666cdd2460fe727eddc5c36020e55
Traderhumba <keith_brooks38@hotmail.co.uk>
2026-04-11 11:04:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import requests
 
 app= Flask(_name_)
 
-TELEGRAM_TOKEN ="8632587398:AAGnaRtWzU4ZWFuGuZlVaNMQIgdAoYeI8eE"
-CHAT_ID = "5950286852"
+TOKEN ="8632587398:AAGnaRtWzU4ZWFuGuZlVaNMQIgdAoYeI8eE"
+CHAT = "5950286852"
 
-def send_telegram(message):
-    url ="https://api.telegram.org/bot" + TELEGRAM_TOKEN + "/sendMessage"
-    requests.post(url, json={"chat_id": CHAT_ID, "text": message})
+def send_msg(msg):
+    url ="https://api.telegram.org/bot" + TOKEN + "/sendMessage"
+    requests.post(url, json={"chat_id": CHAT "text": msg})
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json(silent=True) or {}
     action = data.get("action", "UNKNOWN")
     symbol = data.get("symbol", "UNKNOWN")
-    message = "ALERT! Action: " + action + " Symbol: " + symbol
+    send_msg("ALERT! Action: " + action + " Symbol: " + symbol
     return "OK", 200
     if _name_ ="_main_":
     app.run(host="0.0.0.0", port=5000)
